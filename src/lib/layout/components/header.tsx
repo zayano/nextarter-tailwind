25da1d05ea7febe60f2009653a08df4eeb7b6de6
Reza Aditya Sutrisno <adityareza46@gmail.com>
2025-06-01 10:42:56 +0700

    feat(layout): add reload page

--- src/lib/layout/components/header.tsx
+++ src/lib/layout/components/header.tsx
@@ -1,10 +1,20 @@
+'use client';
 import { ThemeToggle } from '@/lib/components/theme-toggle';
 
 export const Header = () => {
+  const handleLogoClick = () => {
+    window.location.reload();
+  };
+
   return (
     <header className="bg-base-100/80 sticky top-0 z-10 w-full backdrop-blur-md">
       <section className="wrapper mx-auto flex items-center justify-between py-2">
-        <div className="mr-auto text-2xl text-black dark:text-white">LOGO</div>
+        <div
+          className="mr-auto cursor-pointer text-2xl text-black transition-opacity hover:opacity-80 dark:text-white"
+          onClick={handleLogoClick}
+        >
+          LOGO
+        </div>
         <div className="ml-auto">
           <ThemeToggle />
         </div>
