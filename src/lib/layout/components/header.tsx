9b6efee65c99934681da1f53c3a6318811c19595
Reza Aditya Sutrisno <adityareza46@gmail.com>
2025-06-01 10:33:36 +0700

    feat(layout): add logo

--- src/lib/layout/components/header.tsx
+++ src/lib/layout/components/header.tsx
@@ -4,6 +4,7 @@ export const Header = () => {
   return (
     <header className="bg-base-100/80 sticky top-0 z-10 w-full backdrop-blur-md">
       <section className="wrapper mx-auto flex items-center justify-between py-2">
+        <div className="mr-auto text-2xl text-black dark:text-white">LOGO</div>
         <div className="ml-auto">
           <ThemeToggle />
         </div>
